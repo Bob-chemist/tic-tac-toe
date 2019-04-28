513664487ca63cd739865930c3a4c553f5c87690
Bob_chemist <bob_chemist@mail.ru>
2019-04-29 01:45:06 +0300

    Almost ready. Logic is working. Some styling needed

--- src/App.js
+++ src/App.js
@@ -6,18 +6,44 @@ export default class App extends Component {
   state = {
     turn: true,
     box: Array(9).fill(''),
+    vic: false,
   };
 
   onClickHandler = id => {
-    const { turn, box } = this.state;
+    const { turn, box, vic } = this.state;
     const value = turn ? 'X' : 'O';
-    if (box[id]) return;
+    if (box[id] || vic) return;
     const newBox = [...box.slice(0, id), value, ...box.slice(id + 1)];
 
     this.setState({
       turn: !turn,
       box: newBox,
     });
+
+    this.isVictory(newBox, id, value);
+  };
+
+  victory = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  isVictory = (box, id, value) => {
+    const toCheck = this.victory.filter(arr => arr.includes(id));
+
+    for (let i in toCheck) {
+      const k = toCheck[i];
+
+      if (box[k[0]] === value && box[k[1]] === value && box[k[2]] === value) {
+        this.setState({ vic: true });
+      }
+    }
   };
 
   render() {
@@ -32,6 +58,11 @@ export default class App extends Component {
         />
       );
     }
-    return <div className={classes.App}>{field}</div>;
+    return (
+      <div className={classes.App}>
+        <div className={classes.AppField}>{field}</div>
+        {this.state.vic ? <p>Win!</p> : null}
+      </div>
+    );
   }
 }
