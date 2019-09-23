d8b2440e8b7f4e82323a5bd1dfdcef955006de98
Загидуллин Р.Н <r.zagidullin@ios.icl>
2019-09-23 17:28:59 +0300

    rewrited to hooks

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Square from './Components/Square/Square';
 import classes from './App.module.sass';
 
-export default class App extends Component {
-  state = {
-    turn: true,
-    box: Array(9).fill(''),
-    winComb: [],
-  };
+const App = () => {
+  const [turn, setTurn] = useState(true);
+  const [box, setBox] = useState(Array(9).fill(''));
+  const [winComb, setWinComb] = useState([]);  
 
-  onClickHandler = id => {
-    const { turn, box, winComb } = this.state;
-
+  const onClickHandler = id => {
     const value = turn ? 'X' : 'O';
     if (box[id] || winComb.length !== 0) return;
     const newBox = [...box.slice(0, id), value, ...box.slice(id + 1)];
 
-    this.setState({
-      turn: !turn,
-      box: newBox,
-    });
+    setTurn(!turn);
+    setBox(newBox);
 
-    this.isVictory(newBox, id, value);
+    isVictory(newBox, id, value);
   };
 
-  victory = [
+  const victory = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -35,54 +29,51 @@ export default class App extends Component {
     [2, 4, 6],
   ];
 
-  isVictory = (box, id, value) => {
-    const toCheck = this.victory.filter(arr => arr.includes(id));
+  const isVictory = (box, id, value) => {
+    const toCheck = victory.filter(arr => arr.includes(id));
 
     for (let i in toCheck) {
       const k = toCheck[i];
 
       if (box[k[0]] === value && box[k[1]] === value && box[k[2]] === value) {
-        this.setState({ winComb: k });
+        setWinComb(k);
       }
     }
   };
 
-  restart = () => {
-    this.setState({
-      turn: true,
-      box: Array(9).fill(''),
-      winComb: [],
-    });
+  const restart = () => {
+    setTurn(true);
+    setBox(Array(9).fill(''));
+    setWinComb([]);
   };
+  
+  const field = [];  
 
-  render() {
-    const field = [];
-    const { box, winComb } = this.state;
-
-    for (let i = 0; i < 9; i++) {
-      let color = 'black';
-      if (winComb.includes(i)) {
-        color = 'red';
-      }
-      field.push(
-        <Square
-          key={i}
-          id={i}
-          onClickHandler={this.onClickHandler}
-          value={box[i]}
-          color={color}
-        />
-      );
+  for (let i = 0; i < 9; i++) {
+    let color = 'black';
+    if (winComb.includes(i)) {
+      color = 'red';
     }
-
-    return (
-      <div className={classes.App}>
-        {winComb.length !== 0 ? <p>Victory!</p> : null}
-        <div className={classes.AppField}>{field}</div>
-        <button className={classes.Restart} onClick={this.restart}>
-          Restart
-        </button>
-      </div>
+    field.push(
+      <Square
+        key={i}
+        id={i}
+        onClickHandler={(id) => onClickHandler(id)}
+        value={box[i]}
+        color={color}
+      />
     );
   }
+
+  return (
+    <div className={classes.App}>
+      {winComb.length !== 0 ? <p>Victory!</p> : null}
+      <div className={classes.AppField}>{field}</div>
+      <button className={classes.Restart} onClick={() => restart()}>
+        Restart
+      </button>
+    </div>
+  );  
 }
+
+export default App;
--- src/Components/Square/Square.js
+++ src/Components/Square/Square.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import classes from './Square.module.sass';
 
-const Square = props => {
-  const { id, onClickHandler, value, color } = props;
-  const style = { color };
+const Square = (props) => {
+    const { id, onClickHandler, value, color } = props;
+    const style = { color };
 
-  return (
-    <div
-      id={id}
-      style={style}
-      className={classes.Square}
-      onClick={() => onClickHandler(id)}
-    >
-      {value}
-    </div>
-  );
+    return (
+        <div
+            id={id}
+            style={style}
+            className={classes.Square}
+            onClick={() => onClickHandler(id)}
+        >
+            {value}
+        </div>
+    );
 };
 
 export default Square;
